583ca5010b66093058a2e4581c91d0f476300291
Esteban Zapata <jezg1992@hotmail.com>
2016-02-12 06:10:38 -0500

    Ejercicios Java Lambda
    - Se realizan verificaciones en el ejercicio de lambda de nombres
    
    - Se agregan soluciones a los siguientes ejercicios:
    
        Mostrar: 
            el nombre del fabricante y modelo de los vehiculo que:
                sea carro
                sea manual
                tenga un caballaje entre 100 y 160
                traccion trasera ( 2-Wheel Drive, Rear)
            Organizado por caballaje
    
    
        por cada fabricante mostrar los modelos que hace:
            Ej:  Chevrolet: Sprint, Spark, Optra, Vitara 
            sin repetir, sin tener en cuentas variantes, es decir que vitara manual y automatico es simplemente un vitara.
    
    
        cuantos vehículos hay por cantidad de cambios (# of gears)
            ej: 
                4 gears:  2.456
                5 gears: 12.446
    
    
        Top 10 de los vehiculos con mas caballaje
            mostrar fabricante, modelo y caballaje
            Solo concursa un vehiculo por fabricante
            Ej:
                Ferrary Enzo 800 HP
                Mercedes F1 799 HP
    
    
        Por cada fabricante y por cada modelo mostrar el numero que existe:
            Ej: 
                Aston Martin, DB9 => 2
                Aston Martin, Rapide S => 2
                Audi, A4 => 15
    
    
        Top 10 de las marcas con mas vehiculos con caja manual de 6 velocidades, indicando la cantidad
            EJ:
                Mercedes: 300
                BMW: 270
                Renault: 260
    
    
    Falta:
    
        Listar cantidad de vehiculos por rango de caballos de fuerza por marca:
            Los rangos van de 0-100,  101-200, 201-300, 301-en adelante
            EJ:
                BMW
                    0-100: 12
                    101-200: 345
                    201-300: 45
                    301.... : 21
                CHEVROLET
                    0-100: 33
                    101-200: 44
                    201-300: 66
                    301.... : 76

--- src/com/lambda/NamesTest.java
+++ src/com/lambda/NamesTest.java
@@ -16,11 +16,13 @@ import java.util.List;
  */
 public class NamesTest {
 
-    private final Comparator<String> byLastName = (name1, name2) -> name2.split("\\s+")[1].compareTo(name1.split("\\s+")[1]);
+    private final Comparator<String> byLastName = (name1, name2) -> { 
+        String[] nameArray1 = name1.trim().split("\\s+");
+        String[] nameArray2 = name2.trim().split("\\s+");
+        return (nameArray2.length > 1 ? nameArray2[1]: "").compareTo(nameArray1.length > 1 ? nameArray1[1]: "");
+    };
 
-    private final Comparator<String> byLength = (name1, name2) -> Integer.compare(name1.length(), name2.length());
-
-   
+    private final Comparator<String> byLength = (name1, name2) -> Integer.compare(name1.replaceAll(" ", "").trim().length(), name2.replaceAll(" ", "").trim().length());
 
     public void execute() {
         List<String> nameList = Arrays.asList("Barbara Lewis", "Maria Patterson", "Heather Evans", "Kelly Butler", "Jonathan Morgan", "Kevin Barnes", "Nicholas Hill", "Michael Coleman", "Donna Howard", "Jean Gonzalez");
@@ -28,21 +30,23 @@ public class NamesTest {
         System.out.println("=========== Names order by length ==========");
         nameList.stream()
                 .sorted(byLength)
-                .forEach(name -> System.out.println(name + " --- " + name.length()));
+                .forEach(name -> System.out.println(name.replaceAll("\\s+", " ").trim() + " --- " + name.replaceAll("\\s+", " ").trim().length()));
 
         System.out.println("");
         System.out.println("");
         System.out.println("=========== Names order by lastname ==========");
         nameList.stream()
                 .sorted(byLastName)
-                .forEach(System.out::println);
-        
+                .forEach(name -> System.out.println(name.replaceAll("\\s+", " ").trim()));
+
         System.out.println("");
         System.out.println("");
-        System.out.println("=========== First lastname and ==========");
+        System.out.println("=========== First lastname and name ==========");
         nameList.stream()
-                .map(name -> name.split("\\s+")[1] + " " + name.split("\\s+")[0])
-                .forEach(System.out::println);
+                .map((name) -> {
+                    String[] nameArray = name.trim().split("\\s+");
+                    return nameArray.length > 1 ? (nameArray[1] + " " + nameArray[0]): nameArray[0];
+                }).forEach(System.out::println);
     }
 
 }
